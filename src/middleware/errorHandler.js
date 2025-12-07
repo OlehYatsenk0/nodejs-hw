2c7d4569ca131528d6837c768feac07c5ab7d74a
Oleh Yatsenko <yatsenko.prog@gmail.com>
2025-12-07 13:08:18 +0200

    fix: improved errorHandler

--- src/middleware/errorHandler.js
+++ src/middleware/errorHandler.js
@@ -1,7 +1,16 @@
+import { HttpError } from "http-errors";
+
 export const errorHandler = (err, req, res, next) => {
+  if (err instanceof HttpError) {
+    return res.status(err.statusCode || err.status || 500).json({
+      message: err.message || err.name,
+    });
+  }
+
+  // Обробка звичайних помилок
   const status = err.status || 500;
 
-  res.status(status).json({
-    message: err.message || 'Server error',
+  return res.status(status).json({
+    message: err.message || "Server error",
   });
 };
